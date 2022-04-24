6e2d3abf02d3044391754551398b446707ac7abe
wadsongarbes <nenogarbes@hotmail.com>
2022-04-24 18:13:12 -0300

    wrap integrate in a function

--- app/integrator.py
+++ app/integrator.py
@@ -1,18 +1,24 @@
+from typing import final
 from app import db
 from app.models import Question
 
-question = Question(
-    subject="Português", 
-    body="A internet proporcionou o surgimento de novos paradigmas sociais e impulsionou a modificação de outros já estabelecidos nas esferas da comunicação e da informação. A principal consequência criticada na tirinha sobre esse processo é a",
-    img_link="https://static.todamateria.com.br/upload/qu/es/questao19-cke.jpg",
-    alternativeA="criação de memes.",
-    alternativeB="ampliação da blogosfera.",
-    alternativeC="supremacia das ideias cibernéticas.",
-    alternativeD="comercialização de pontos de vista",
-    alternativeE="banalização do comércio eletrônico.",
-    answer="d) comercialização de pontos de vista."
-)
-
-db.session.add(question)
-db.session.commit()
-db.session.close()
+def integrate_questions():
+    question = Question(
+        subject="Português", 
+        body="A internet proporcionou o surgimento de novos paradigmas sociais e impulsionou a modificação de outros já estabelecidos nas esferas da comunicação e da informação. A principal consequência criticada na tirinha sobre esse processo é a",
+        img_link="https://static.todamateria.com.br/upload/qu/es/questao19-cke.jpg",
+        alternativeA="criação de memes.",
+        alternativeB="ampliação da blogosfera.",
+        alternativeC="supremacia das ideias cibernéticas.",
+        alternativeD="comercialização de pontos de vista",
+        alternativeE="banalização do comércio eletrônico.",
+        answer="d) comercialização de pontos de vista."
+    )
+    try:
+        db.session.add(question)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(f"ERROR: {e} - Rollback trigged")
+    finally:
+        db.session.close()
