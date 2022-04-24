913a4fe59a89d45c17ca59f5a18f40452c811332
wadsongarbes <nenogarbes@hotmail.com>
2022-04-24 18:05:56 -0300

    shell context

--- base10.py
+++ base10.py
@@ -1,4 +1,13 @@
 from app import create_app
+from app.models import Question
 
 
 app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Question": Question
+    }
